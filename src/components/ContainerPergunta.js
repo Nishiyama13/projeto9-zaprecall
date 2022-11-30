015d4e2133380f3996464335804a126fb13f0de6
Nishiyama13 <nishiyamaeel@gmail.com>
2022-11-30 17:24:56 -0300

    pergunta aberta Styled components

--- src/components/ContainerPergunta.js
+++ src/components/ContainerPergunta.js
@@ -1,10 +1,16 @@
 import styled from "styled-components";
+import seta_virar from "../assets/seta_virar.png";
+
 export default function ContainerPergunta() {
   return (
     <>
       <PerguntaFechada>
         <p>Pergunta 1</p>
       </PerguntaFechada>
+      <PerguntaAberta>
+        <p>O que é o JSX?</p>
+        <img src={seta_virar} alt="" />
+      </PerguntaAberta>
     </>
   );
 }
@@ -20,4 +26,38 @@ const PerguntaFechada = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  p {
+    font-family: "Recursive";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    color: #333333;
+  }
+`;
+//.pergunta-aberta {}
+const PerguntaAberta = styled.div`
+  width: 300px;
+  margin: 12px;
+  padding: 15px;
+  min-height: 100px;
+  background: #ffffd5;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  font-family: "Recursive";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 22px;
+  color: #333333;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  img {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+  }
 `;
